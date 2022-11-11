1a2d3204ca1d43e15c38ae263c72adf4a0037c19
RaymondNemr <raymondprogrammer@gmail.com>
2022-11-11 07:45:06 -0300

    migrando base de dados para postgresql

--- School_methods.py
+++ School_methods.py
@@ -1,13 +1,12 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from database import Student
-from database import Grade
+import Student_and_Grade
+import pg8000
 import json
 
-Base = declarative_base()
+Base = Student_and_Grade.Base
 
-engine = create_engine('sqlite:///new_class.sqlite', echo=False)
+engine = create_engine('postgresql+pg8000://postgres:r4ym0nd@localhost:5432/bdprd')
 
 Base.metadata.create_all(engine)
 
@@ -20,7 +19,7 @@ class School:
 
 
     def set_student(self, student_name, student_age):
-        student = Student(name = student_name, age = student_age)
+        student = Student_and_Grade.Student(name = student_name, age = student_age)
         
         self.session.add(student)
         self.session.commit()
@@ -30,7 +29,7 @@ class School:
     
     def set_student_grade(self, student_id, grade, subject):
 
-        grade = Grade(student_id = student_id, grade = grade, subject = subject)      
+        grade = Student_and_Grade.Grade(student_id = student_id, grade = grade, subject = subject)      
         self.session.add(grade)
         self.session.commit()
 
@@ -38,7 +37,7 @@ class School:
 
     
     def set_grade(self, grade_id, new_grade):
-        result = self.session.query(Grade).get(grade_id)
+        result = self.session.query(Student_and_Grade.Grade).get(grade_id)
 
         result.grade = new_grade
         self.session.commit()
@@ -47,7 +46,7 @@ class School:
 
 
     def get_student_by_name(self, student_name):
-        result = self.session.query(Student).filter(Student.name == student_name)        
+        result = self.session.query(Student_and_Grade.Student).filter(Student_and_Grade.Student.name == student_name)        
         students_array = [{'name':s.name, 'age':s.age} for s in result]
         dictionary = {'students':students_array}
 
@@ -55,13 +54,13 @@ class School:
     
     
     def get_student_by_id(self, student_id):
-        result = self.session.query(Student).get(student_id)
+        result = self.session.query(Student_and_Grade.Student).get(student_id)
         
         return json.dumps({'name':result.name, 'age':result.age})
     
     
     def get_student_grades(self, id):
-        result = self.session.query(Grade).filter(Grade.student_id==id)
+        result = self.session.query(Student_and_Grade.Grade).filter(Student_and_Grade.Grade.student_id==id)
         grades_array = [{'subject':s.subject, 'grade':s.grade} for s in result]
         dictionary = {'grades':grades_array}
 
@@ -69,7 +68,7 @@ class School:
 
     
     def get_grade(self, grade_id):
-        result = self.session.query(Grade).get(grade_id)
+        result = self.session.query(Student_and_Grade.Grade).get(grade_id)
 
         return json.dumps({'subject':result.subject, 'grade':result.grade})
 
